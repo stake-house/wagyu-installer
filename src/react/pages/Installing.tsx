4dbe342a91743b3f6a86f24e28ed5ab8b9c657b2
Colfax Selby <colfax.selby@gmail.com>
2021-03-26 08:59:29 -0400

    CSS

--- src/react/pages/Installing.tsx
+++ src/react/pages/Installing.tsx
@@ -53,6 +53,7 @@ const LoadingSpinner = styled.div`
   border: 16px solid #f3f3f3; /* Light grey */
   border-top: 16px solid #3498db; /* Blue */
   border-radius: 50%;
+  margin-top: 30px;
   width: 120px;
   height: 120px;
   animation: ${rotate} 2s linear infinite;
