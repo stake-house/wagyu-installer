5b8805365a83c77eaab2d75b0f7a92c74d03e4f9
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-08-10 16:59:08 -0400

    format SystemCheck, clean up imports, consolidate a few render functions

--- src/react/components/SystemCheck.tsx
+++ src/react/components/SystemCheck.tsx
@@ -1,20 +1,15 @@
-import {
-  DarkBlue,
-  Heading,
-  MainContent
-} from "../colors";
-import React, { useEffect, useState } from "react";
-
-import Footer from "../components/Footer";
-import { isRocketPoolInstalled } from "../commands/RocketPool";
-import styled from "styled-components";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { DarkBlue, Heading, MainContent } from '../colors';
+import Footer from './Footer';
+import { isRocketPoolInstalled } from '../commands/RocketPool';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height:100vh;
+  min-height: 100vh;
 `;
 
 const LandingHeader = styled.div`
@@ -23,7 +18,7 @@ const LandingHeader = styled.div`
   margin-top: 50;
   color: ${Heading};
   max-width: 550;
-  flex-grow:1;
+  flex-grow: 1;
 `;
 
 const Content = styled.div`
@@ -34,7 +29,7 @@ const Content = styled.div`
 `;
 
 const Advanced = styled.div`
-  font-size:15;
+  font-size: 15;
   color: ${DarkBlue};
   max-width: 550;
   cursor: pointer;
@@ -48,22 +43,18 @@ const ResultsTable = styled.table`
   color: white;
 `;
 
-const SystemCheck = () => {
+export const SystemCheck = () => {
   const [showAdvanced, setShowAdvanced] = useState(false);
-
   const [arbitraryTest, setArbitraryTest] = useState(true);
   const [rocketPoolInstalled, setRocketPoolInstalled] = useState(false);
   const [systemStatus, setSystemStatus] = useState(false);
 
   useEffect(() => {
     runSystemCheck();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    setSystemStatus(
-      arbitraryTest &&
-      !rocketPoolInstalled
-    );
+    setSystemStatus(arbitraryTest && !rocketPoolInstalled);
   }, [arbitraryTest, rocketPoolInstalled]);
 
   const runSystemCheck = () => {
@@ -77,24 +68,14 @@ const SystemCheck = () => {
 
     // TODO: add instructions/links for install/fix if a test fails
     // TODO: create a loading state and only render once the test is finished
-  }
+  };
 
   const resultToIcon = (result: boolean): string => {
     // TODO: make these icons - green check and red X
-    return result ? "Pass" : "Fail";
-  }
+    return result ? 'Pass' : 'Fail';
+  };
 
-  const toggleShowAdvanced = () => {
-    setShowAdvanced(!showAdvanced);
-  }
-
-  const renderRocketPoolInstalledContent = () => {
-    return (
-      <Content>
-        It looks like you already have Rocket Pool installed.  Let's hop over to check the status of your node.
-      </Content>
-    );
-  }
+  const toggleShowAdvanced = () => setShowAdvanced(!showAdvanced);
 
   const renderSystemCheckResultsTable = () => {
     // TODO: make this dynamic
@@ -108,7 +89,9 @@ const SystemCheck = () => {
         </thead>
         <tbody>
           <tr>
-            <td>Rocket Pool <i>not</i> installed</td>
+            <td>
+              Rocket Pool <i>not</i> installed
+            </td>
             <td>{resultToIcon(!rocketPoolInstalled)}</td>
           </tr>
           {/* This test is used for development purposes, so we can toggle different states easily */}
@@ -118,39 +101,8 @@ const SystemCheck = () => {
           </tr> */}
         </tbody>
       </ResultsTable>
-    )
-  }
-
-  const renderSystemReady = () => {
-    return (
-      <div>
-        We are good to go.  We will pick a random eth1 and eth2 client to install in order to promote client diversity.
-        <br/>
-        <br/>
-        <Advanced onClick={toggleShowAdvanced}>Advanced</Advanced>
-        { // TODO: fix jump when this is clicked, or redisgn this alltogether
-          // TODO: add pivoting dropdown arrow to signify menu opening and closing?
-          showAdvanced ?
-          <div>
-            <br />
-            <em>This is where we will allow users to pick their client, customize params, etc, however it is not yet implemented.</em>
-            <br /><br />
-            Come join the team and help out :)
-          </div>
-            :
-          <div></div>
-        }
-      </div>
     );
-  }
-
-  const renderCantProceed = () => {
-    return (
-      <div>
-        Unfortunately your system does not meet the requirements so we cannot proceed :(
-      </div>
-    )
-  }
+  };
 
   const renderSystemCheckContent = () => {
     return (
@@ -158,37 +110,89 @@ const SystemCheck = () => {
         We did some system checks, here are the results:
         <br />
         <br />
-        { renderSystemCheckResultsTable() }
+        {renderSystemCheckResultsTable()}
         <br />
         <br />
-        { systemStatus ? renderSystemReady() : renderCantProceed() }
+        {systemStatus ? (
+          <div>
+            We are good to go. We will pick a random eth1 and eth2 client to
+            install in order to promote client diversity.
+            <br />
+            <br />
+            <Advanced onClick={toggleShowAdvanced}>Advanced</Advanced>
+            {
+              // TODO: fix jump when this is clicked, or redesign this altogether
+              // TODO: add pivoting dropdown arrow to signify menu opening and closing?
+
+              showAdvanced ? (
+                <div>
+                  <br />
+                  <em>
+                    This is where we will allow users to pick their client,
+                    customize params, etc, however it is not yet implemented.
+                  </em>
+                  <br />
+                  <br />
+                  Come join the team and help out :)
+                </div>
+              ) : (
+                <div />
+              )
+            }
+          </div>
+        ) : (
+          <div>
+            Unfortunately your system does not meet the requirements so we
+            cannot proceed :(
+          </div>
+        )}
       </Content>
     );
-  }
+  };
 
   const renderContent = () => {
     if (rocketPoolInstalled) {
-      return renderRocketPoolInstalledContent();
+      return (
+        <Content>
+          It looks like you already have Rocket Pool installed. Let's hop over
+          to check the status of your node.
+        </Content>
+      );
     } else {
       return renderSystemCheckContent();
     }
-  }
+  };
 
   const renderFooter = () => {
     if (systemStatus) {
       return (
-        <Footer backLink={"/"} backLabel={"Back"} nextLink={"/installing"} nextLabel={"Install"} />
-      )
+        <Footer
+          backLink={'/'}
+          backLabel={'Back'}
+          nextLink={'/installing'}
+          nextLabel={'Install'}
+        />
+      );
     } else if (rocketPoolInstalled) {
       return (
-        <Footer backLink={"/"} backLabel={"Back"} nextLink={"/status"} nextLabel={"Status"} />
-      )
+        <Footer
+          backLink={'/'}
+          backLabel={'Back'}
+          nextLink={'/status'}
+          nextLabel={'Status'}
+        />
+      );
     } else {
       return (
-        <Footer backLink={"/"} backLabel={"Back"} nextLink={""} nextLabel={""} />
-      )
+        <Footer
+          backLink={'/'}
+          backLabel={'Back'}
+          nextLink={''}
+          nextLabel={''}
+        />
+      );
     }
-  }
+  };
 
   return (
     <Container>
@@ -196,7 +200,5 @@ const SystemCheck = () => {
       {renderContent()}
       {renderFooter()}
     </Container>
-  )
-}
-
-export default SystemCheck;
+  );
+};
