43e162f998cf16c0bec13d0a6321c96c61268f84
Stefan <stefan@bitfly.at>
2022-08-10 23:49:07 +0200

    dashboard layout

--- src/react/pages/SystemOverview.tsx
+++ src/react/pages/SystemOverview.tsx
@@ -10,35 +10,89 @@ const MainGrid = styled(Grid)`
   text-align: center;
 `;
 
+const Card = styled(Grid)`
+  background: rgba(0, 94, 86, 0.2);
+  border: 1px solid rgba(255, 166, 0, 0.3);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+
 type SystemOverviewProps = {
   installationDetails: InstallDetails
 }
 
 const SystemOverview: FC<SystemOverviewProps> = (props): ReactElement => {
   return (
-    <MainGrid container spacing={5} direction="column">
-      <Grid item container>
-        <Grid item xs={10} />
-        <Grid item xs={2}>
-          <Typography variant="caption" style={{ color: "gray" }}>
-            Network: {props.installationDetails.network}
-          </Typography>
+    <MainGrid container spacing={0} direction="column">
+     <Grid flex={"0 0"} container item>
+      <h1>Overview</h1>
+     </Grid>
+     <Grid flex={"1 1"} item container>
+      <Grid xs={6} padding={1} container item direction={"column"}>
+        <Grid item>
+          <h3>Beacon Node</h3>
         </Grid>
+        <Card item>
+          <div>Status</div>
+          <div>Remaining</div>
+          <div>DB Path</div>
+        </Card>
       </Grid>
-      <Grid item>
-        <Typography variant="h1">
-          {/* System Overview */}
-          Installation Complete
-        </Typography>
-        <ul style={{ margin: '0 auto', width: '350px', marginTop: '3rem', textAlign: 'left'}}>
-          <li>Network: {props.installationDetails.network}</li>
-          <li>Consensus Client: {props.installationDetails.consensusClient}</li>
-          <li>Execution Client: {props.installationDetails.executionClient}</li>
-        </ul>
+      <Grid xs={6} padding={1} container item direction={"column"}>
+        <Grid item>
+          <h3>Validator</h3>
+        </Grid>
+        <Card item>
+          <div>Add validator keys</div>
+          <div></div>
+          <div></div>
+        </Card>
       </Grid>
+      <Grid xs={6} padding={1} container item direction={"column"}>
+        <Grid item>
+          <h3>Execution Node</h3>
+        </Grid>
+        <Card item>
+          <div>Status</div>
+          <div>Remaining</div>
+          <div>DB Path</div>
+        </Card>
+      </Grid>
+      <Grid xs={6} padding={1} container item direction={"column"}>
+        <Grid item>
+          <h3>System</h3>
+        </Grid>
+        <Card item>
+          <div>Storage growth</div>
+          <div>Storage left</div>
+          <div>RAM</div>
+          <div>CPU</div>
+        </Card>
+      </Grid>
+     </Grid>
+
       <VersionFooter />
     </MainGrid>
   );
 };
 
 export default SystemOverview;
+
+
+{/* <Grid item container>
+<Grid item xs={10} />
+<Grid item xs={2}>
+  <Typography variant="caption" style={{ color: "gray" }}>
+    Network: {props.installationDetails.network}
+  </Typography>
+</Grid>
+</Grid>
+<Grid item>
+<Typography variant="h1">
+  Installation Complete
+</Typography>
+<ul style={{ margin: '0 auto', width: '350px', marginTop: '3rem', textAlign: 'left'}}>
+  <li>Network: {props.installationDetails.network}</li>
+  <li>Consensus Client: {props.installationDetails.consensusClient}</li>
+  <li>Execution Client: {props.installationDetails.executionClient}</li>
+</ul>
+</Grid> */}
