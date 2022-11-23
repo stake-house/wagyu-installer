1386443be2620963bc61481dc0cd4c59632ff0a8
Stefan <stefan@bitfly.at>
2022-11-23 23:25:49 +0100

    format pubkey func

--- src/utility.ts
+++ src/utility.ts
@@ -4,3 +4,11 @@ export function Capitalize(text: string) {
     }
     return text
 }
+
+export function FormatPubkey(pubkey: string): string {
+    if(pubkey && pubkey.length > 6) {
+        // return pubkey.substring(0, 3) + "…" + pubkey.substring(-3, 3)
+        return pubkey.substring(0, 6) + "..." + pubkey.substring(pubkey.length -6)
+    }
+    return pubkey 
+}
